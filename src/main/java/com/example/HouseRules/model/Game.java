6cb92c1c2d8c39190e6d3388c6bf871367cabaff
Bernard Strong <bstrong71@gmail.com>
2017-10-14 14:38:26 -0400

    added game class to model

---
+++ src/main/java/com/example/HouseRules/model/Game.java
@@ -0,0 +1,106 @@
+package com.example.HouseRules.model;
+
+import javax.persistence.*;
+import java.util.Arrays;
+
+@Entity
+@Table(name = "games")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String category;
+
+    @Column
+    private String objective;
+
+    @Column
+    private String rules;
+
+    @Column
+    private byte[] image;
+
+    public Game() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getObjective() {
+        return objective;
+    }
+
+    public void setObjective(String objective) {
+        this.objective = objective;
+    }
+
+    public String getRules() {
+        return rules;
+    }
+
+    public void setRules(String rules) {
+        this.rules = rules;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Game game = (Game) o;
+
+        return getId() == game.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return getId();
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", category='" + category + '\'' +
+                ", objective='" + objective + '\'' +
+                ", rules='" + rules + '\'' +
+                ", image=" + Arrays.toString(image) +
+                '}';
+    }
+}
