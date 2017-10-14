8696b8800f433f155112e8c431e96c324e2841e5
Bernard Strong <bstrong71@gmail.com>
2017-10-14 11:46:35 -0400

    initial commit

---
+++ src/main/java/com/example/HouseRules/HouseRulesApplication.java
@@ -0,0 +1,12 @@
+package com.example.HouseRules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseRulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseRulesApplication.class, args);
+	}
+}
