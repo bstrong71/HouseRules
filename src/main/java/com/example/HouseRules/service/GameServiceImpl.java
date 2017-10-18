3a0d56c3b185b276ac8a0c4a9fd48f7b3b0bdee4
Bernard Strong <bstrong71@gmail.com>
2017-10-17 21:55:55 -0400

    removed unused import

--- src/main/java/com/example/HouseRules/service/GameServiceImpl.java
+++ src/main/java/com/example/HouseRules/service/GameServiceImpl.java
@@ -5,7 +5,6 @@ import com.example.HouseRules.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
