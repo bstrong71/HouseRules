3880ba80f1d8d6fa421a3d2024a324c7132c1bfe
Bernard Strong <bstrong71@gmail.com>
2017-10-22 16:01:56 -0400

    added config folder with WebConfig class to enable CORS requests from any origin

---
+++ src/main/java/com/example/HouseRules/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.HouseRules.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+// This global configuration enables CORS requests from any origin to any endpoint in the application
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
